60d307640fd06d67749558e55674908481f50fe3
Kshitiz <kshitizjain2020@gmail.com>
2026-01-10 14:14:11 +0530

    Basic gsap setup for animations

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,10 +1,14 @@
+import { useGSAP } from "@gsap/react";
 import { OrbitControls, useAnimations, useGLTF, useTexture } from "@react-three/drei";
 import {  useThree } from "@react-three/fiber";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import React, { useEffect } from "react";
 import { MeshMatcapMaterial } from "three";
 import * as THREE from 'three'
 const Dog = () => {
-  
+    gsap.registerPlugin(useGSAP())
+    gsap.registerPlugin(ScrollTrigger)
+    
     const model = useGLTF("/models/dog.drc.glb")
     useThree(({camera,gl,scene})=>{
         camera.position.z = 0.5,
@@ -25,8 +29,12 @@ const Dog = () => {
     // texture.normalMap.flipY = false
     // texture.sampleMatCap.colorSpace = THREE.SRGBColorSpace
     
-    const [normalMap,sampleMatCap,branchNormalMap,branchMap] = (useTexture(["/images/dog_normals.png","/matcap/mat-2.png","/images/branches_normals.jpg","/images/branches_diffuse.jpg"])).map(texture=>{
+    const [normalMap,sampleMatCap] = (useTexture(["/images/dog_normals.png","/matcap/mat-2.png"])).map(texture=>{
         texture.flipY = false,
+        texture.colorSpace = THREE.SRGBColorSpace
+        return texture
+    })
+    const [branchNormalMap,branchMap] = (useTexture(["/images/branches_normals.jpg","/images/branches_diffuse.jpg"])).map(texture=>{
         texture.colorSpace = THREE.SRGBColorSpace
         return texture
     })
@@ -42,11 +50,7 @@ const Dog = () => {
     model.scene.traverse((child)=>{
         if (child.name.includes("DOG")){
             child.material = dogMaterial
-        }
-
-        if (child.name.includes("BRANCH")){
-            child.material = branchMaterial
-        }
+        } else child.material = branchMaterial
         
     })
 
@@ -55,7 +59,6 @@ const Dog = () => {
     
       <primitive object={model.scene}  position={[0.2,-0.6,0]} rotation={[0,0.5,0]} />
       <directionalLight position={[0,5,5]} color={0xFFFFFF} intensity={10} />
-        <OrbitControls/>
     </>
   );
 };
